810e208513c12123682cd92b1b48dfdbbab2742a
Endarked <Endarked@gmail.com>
2014-01-07 16:03:17 +0100

    decent pogo lol

--- Escape/src/model/backbone/algorithm/PogoPeopleAlgorithm.java
+++ Escape/src/model/backbone/algorithm/PogoPeopleAlgorithm.java
@@ -3,6 +3,10 @@ package model.backbone.algorithm;
 import java.util.Random;
 
 import model.backbone.agent.Agent;
+import model.backbone.agent.Agent.DestinationType;
+import model.backbone.building.helpers.Point;
+import model.backbone.utils.AlgorithmUtilities;
+import model.backbone.utils.CollisionUtils;
 
 public class PogoPeopleAlgorithm extends Algorithm {
 
@@ -10,12 +14,21 @@ public class PogoPeopleAlgorithm extends Algorithm {
 	
 	@Override
 	public void setAgentDestination(Agent agent) {
-		int x, y;
-		x = rand.nextInt(2)-1;
-		y = rand.nextInt(2)-1;
-		
-		agent.getLocation().setX(agent.getLocation().getX() + x);
-		agent.getLocation().setY(agent.getLocation().getY() + y);
+		agent.setDestination(DestinationType.None,agent.getLocation());
 	}
 
+	@Override
+	public void setAgentDirection(Agent agent) {
+		int x, y;
+		x = rand.nextInt(3)-1;
+		y = rand.nextInt(3)-1;
+		agent.setDirection(new Point(x,y));
+	}
+	
+	@Override
+	public void moveAgent(Agent agent) {
+		if (CollisionUtils.canIMoveThere(agent, agent.getDirection().x, agent.getDirection().y)) {
+			agent.setLocation(new Point(agent.getLocation().x+agent.getDirection().x,agent.getLocation().y+agent.getDirection().y));			
+		}
+	}
 }
